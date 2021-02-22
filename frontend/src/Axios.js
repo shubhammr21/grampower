4dccc9b1299fa322933a860557c4b46bad3bb42f
shubham <shubhammr21@gmail.com>
2021-02-22 17:40:01 +0530

    axios link, setting true

--- frontend/src/Axios.js
+++ frontend/src/Axios.js
@@ -2,7 +2,7 @@
 
 import Axios from "axios"
 
-const baseURL = "https://grammpower.herokuapp.com" /* "http://192.168.43.91:8000/" */
+const baseURL = "127.0.0.1:8000" /* "http://192.168.43.91:8000/" */
 
 Axios.defaults.baseURL = baseURL
 
