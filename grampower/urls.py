5ebdd7d8c84a864c2811ea7295901dae78b0bbbf
shubham <shubhammr21@gmail.com>
2021-02-23 09:38:07 +0530

    media url update

--- grampower/urls.py
+++ grampower/urls.py
@@ -50,11 +50,11 @@ contact=openapi.Contact(email="shubhammr21@gmail.com"),
         ), name='schema-redoc'),
     ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns += [path('__debug__/', include(debug_toolbar.urls)), ]
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
     # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
